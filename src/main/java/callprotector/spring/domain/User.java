03a0fc092c9ed82204fcc9f4b25e39cf3c49949e
SONG <dlthdal07@gmail.com>
2025-05-07 21:28:17 +0900

    Feat: User Entity 구현

--- src/main/java/callprotector/spring/domain/User.java
+++ src/main/java/callprotector/spring/domain/User.java
@@ -1,4 +1,46 @@
 package callprotector.spring.domain;
 
-public class User {
+import callprotector.spring.domain.enums.Department;
+import callprotector.spring.domain.enums.Position;
+import callprotector.spring.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 16)
+    private String password;
+
+    private String profileImg;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Department department;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Position position;
+
+    @Column(nullable = false)
+    private Long totalCall = 0L;
+
+    public void increaseCallCount() {
+        this.totalCall += 1;
+    }
+
 }
