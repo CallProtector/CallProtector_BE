98382710510b1b9b4f55cb5cff528cef24fa6a8f
SONG <dlthdal07@gmail.com>
2025-07-11 01:09:23 +0900

    🐛 Fix: STT 최종 로그의 abuseType이 저장되지 않던 문제 수정 #34

--- src/main/java/callprotector/spring/repository/CallSttLogRepository.java
+++ src/main/java/callprotector/spring/repository/CallSttLogRepository.java
@@ -3,6 +3,9 @@ package callprotector.spring.repository;
 import callprotector.spring.domain.CallSttLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CallSttLogRepository extends MongoRepository<CallSttLog, String> {
     boolean existsByCallSessionIdAndIsAbuseTrue(Long callSessionId);
+    List<CallSttLog> findByCallSessionIdAndIsAbuseTrue(Long callSessionId);
 }
