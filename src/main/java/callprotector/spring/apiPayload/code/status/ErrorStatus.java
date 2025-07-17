1c54f331eb1a60651a689c06ef904fc8ce86c809
ose0221 <ose0221@naver.com>
2025-07-18 06:22:01 +0900

    Feat: 상담 내역 상세 조회 시 에러 코드 추가

--- src/main/java/callprotector/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/callprotector/spring/apiPayload/code/status/ErrorStatus.java
@@ -19,6 +19,12 @@ public enum ErrorStatus implements BaseErrorCode {
     // User 관련 에러
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4001", "사용자가 없습니다."),
 
+    // Call Session 관련
+    CALL_SESSION_NOT_FOUND(HttpStatus.NOT_FOUND, "CALLSESSION4041", "call session이 존재하지 않습니다."),
+
+    // Call Stt Log 관련
+    CALL_STT_LOG_NOT_FOUND(HttpStatus.NOT_FOUND, "CALLSTTLOG4041", "조건에 맞는 STT LOG가 존재하지 않습니다."),
+
     // 임시 에러
     TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "테스트 에러 입니다.");
 
