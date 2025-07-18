234e303ddf3ab7e58f03a036a4075238ab96cab9
SONG <dlthdal07@gmail.com>
2025-07-18 21:01:45 +0900

    :recycle: Refactor: CallSessionController URI 수정 #51

--- src/main/java/callprotector/spring/web/controller/CallSessionController.java
+++ src/main/java/callprotector/spring/web/controller/CallSessionController.java
@@ -16,13 +16,13 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/sessions")
 public class CallSessionController {
 
     private final CallSessionService callSessionService;
 
     @Operation(summary = "CallSession 생성", description = "Twilio 수신 시 콜세션을 생성하고 발신번호를 저장합니다.")
-    @PostMapping("/call-session")
+    @PostMapping("")
     public ApiResponse<CallSessionResponseDTO.CallSessionMakeDTO> createCallSession(
             @AuthenticationPrincipal String email,
             @RequestBody CallSessionRequestDTO.CallSessionMakeDTO dto) {
@@ -33,7 +33,7 @@ public class CallSessionController {
     }
 
     @Operation(summary = "상담 내역 조회 API", description = "정렬 조건을 선택하여 전체 상담 내역을 조회합니다.")
-    @GetMapping("/sessions")
+    @GetMapping("")
     public ApiResponse<List<CallSessionResponseDTO.CallSessionListDTO>> getCallSessions(
             @Parameter(description = "정렬 기준 필드 (예: createdAt)")
             @RequestParam(defaultValue = "createdAt") String sortBy,
@@ -46,10 +46,10 @@ public class CallSessionController {
     }
 
     @Operation(summary = "callSession 상세 조회", description = "상담 내역 상세 조회 시 callSession을 조회합니다.")
-    @GetMapping("/sessions/{callSessionId}")
+    @GetMapping("/{callSessionId}")
     public ApiResponse<CallSessionResponseDTO.CallSessionDetailResponseDTO> getCallSession(
-        @PathVariable("callSessionId") Long id
-        // TODO: 유저 인증(token) 추가
+            @PathVariable("callSessionId") Long id
+            // TODO: 유저 인증(token) 추가
     ) {
         CallSessionResponseDTO.CallSessionDetailResponseDTO response = callSessionService.getCallSessionDetail(id);
         return ApiResponse.onSuccess(response);
