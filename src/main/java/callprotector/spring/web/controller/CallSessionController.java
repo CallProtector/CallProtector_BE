7435ce5275b60bde834e16ba0107b4c9b7c7216b
SONG <dlthdal07@gmail.com>
2025-07-18 20:57:59 +0900

    :sparkles: Feat: 상담 내역 조회 controller 구현 #51

--- src/main/java/callprotector/spring/web/controller/CallSessionController.java
+++ src/main/java/callprotector/spring/web/controller/CallSessionController.java
@@ -5,14 +5,14 @@ import callprotector.spring.service.CallSessionService.CallSessionService;
 import callprotector.spring.web.dto.request.CallSessionRequestDTO;
 import callprotector.spring.web.dto.response.CallSessionResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,8 +30,19 @@ public class CallSessionController {
         return ApiResponse.onSuccess(CallSessionResponseDTO.CallSessionMakeDTO.builder()
                 .sessionId(sessionId)
                 .build());
+    }
 
+    @Operation(summary = "상담 내역 조회 API", description = "정렬 조건을 선택하여 전체 상담 내역을 조회합니다.")
+    @GetMapping("/sessions")
+    public ApiResponse<List<CallSessionResponseDTO.CallSessionListDTO>> getCallSessions(
+            @Parameter(description = "정렬 기준 필드 (예: createdAt)")
+            @RequestParam(defaultValue = "createdAt") String sortBy,
 
+            @Parameter(description = "정렬 순서: 최신순(desc), 오래된순(asc)")
+            @RequestParam(defaultValue = "desc") String order
+    ) {
+        List<CallSessionResponseDTO.CallSessionListDTO> sessions = callSessionService.getCallSessions(sortBy, order);
+        return ApiResponse.onSuccess(sessions);
     }
 
     @Operation(summary = "callSession 상세 조회", description = "상담 내역 상세 조회 시 callSession을 조회합니다.")
