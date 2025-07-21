a9416ef43a5ecab13dcceaaef932643f3035c7ef
ose0221 <ose0221@naver.com>
2025-07-21 23:42:08 +0900

    CHORE: fastAPI URL 배포 주소로 수정

--- src/main/java/callprotector/spring/CallprotectorApplication.java
+++ src/main/java/callprotector/spring/CallprotectorApplication.java
@@ -3,9 +3,11 @@ package callprotector.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMongoAuditing
 public class CallprotectorApplication {
 
 	public static void main(String[] args) {
