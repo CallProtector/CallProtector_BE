71907c2ad3dd7842364bfbf4eb617151369d5ccc
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-07-23 02:30:50 +0900

    🐛 .
    .

--- src/test/java/com/example/callprotector/CallprotectorApplicationTests.java
+++ src/test/java/com/example/callprotector/CallprotectorApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.callprotector;
 
+import callprotector.spring.CallprotectorApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CallprotectorApplication.class)
 class CallprotectorApplicationTests {
 
 	@Test
