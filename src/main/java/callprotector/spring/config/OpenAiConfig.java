9f8c564f8095d8c13c9b9a652c25d3be014c31a6
SONG <dlthdal07@gmail.com>
2025-07-24 23:15:03 +0900

    :sparkles: Feat: OpenAI 설정 파일 추가 #67

---
+++ src/main/java/callprotector/spring/config/OpenAiConfig.java
@@ -0,0 +1,31 @@
+package callprotector.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+@Configuration
+@Getter
+public class OpenAiConfig {
+
+    @Value("${openai.secret-key}")
+    private String secretKey;
+
+    @Value("${openai.model}")
+    private String model;
+
+    @Bean
+    public HttpHeaders openAiHeaders(@Value("${openai.secret-key}") String secretKey) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setBearerAuth(secretKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return headers;
+    }
+}
+
