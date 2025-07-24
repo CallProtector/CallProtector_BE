3998c7d022e451370fc94cf4313bf5b95a84e10d
SONG <dlthdal07@gmail.com>
2025-07-24 23:19:52 +0900

    :sparkles: Feat: OpenAI 상담 요약 service 구현 #67

--- src/main/java/callprotector/spring/config/OpenAiConfig.java
+++ src/main/java/callprotector/spring/config/OpenAiConfig.java
@@ -2,7 +2,6 @@ package callprotector.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
