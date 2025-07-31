bcf0d830823bdbdbdf4a99c56261bdcbbd0345dc
현지 <102063992+hyeonjichoi21@users.noreply.github.com>
2025-07-31 17:43:14 +0900

    Update CallSttLogIndexer.java

--- src/main/java/callprotector/spring/elasticsearch/CallSttLogIndexer.java
+++ src/main/java/callprotector/spring/elasticsearch/CallSttLogIndexer.java
@@ -15,6 +15,8 @@ import co.elastic.clients.elasticsearch.core.BulkRequest;
 import java.io.IOException;
 import java.util.List;
 
+// 임시 비활성화용 어노테이션 추가
+@ConditionalOnProperty(name = "elasticsearch.enabled", havingValue = "true")
 @Slf4j
 @Component
 @RequiredArgsConstructor
