267038d255da4a1548a78d2f9d4f2e62e8c81ad9
ose0221 <ose0221@naver.com>
2025-08-01 00:00:56 +0900

    :recycle: Refactor: handler 패키지 이동에 따른 import 경로 수정

--- src/main/java/callprotector/spring/config/WebSocketConfig.java
+++ src/main/java/callprotector/spring/config/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package callprotector.spring.config;
 
+import callprotector.spring.handler.SttWebSocketHandler;
+import callprotector.spring.handler.TwilioMediaStreamsHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
