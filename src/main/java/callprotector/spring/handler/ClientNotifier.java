49f5f45166bee8dca8c9c8573fca141f21bd22dc
ose0221 <ose0221@naver.com>
2025-08-01 23:30:45 +0900

    :recycle: Refactor: STT 결과 및 세션 정보 전송 관련 인터페이스

---
+++ src/main/java/callprotector/spring/handler/ClientNotifier.java
@@ -0,0 +1,14 @@
+package callprotector.spring.handler;
+
+import callprotector.spring.web.dto.response.CallSessionResponseDTO;
+import callprotector.spring.web.dto.response.CallSttLogResponseDTO;
+import org.springframework.web.socket.WebSocketSession;
+
+// 클라이언트에게 STT 결과 및 세션 정보를 WebSocket을 통해 전송하는 기능을 추상화하는 인터페이스
+public interface ClientNotifier {
+	void sendSttToClient(Long userId, CallSttLogResponseDTO sttLogResponseDTO);
+	void sendSttToClient(Long userId, Object payload);
+	void sendSessionInfoToClient(Long userId, CallSessionResponseDTO.CallSessionInfoDTO sessionInfo);
+	void sendUpdateAbuseCntToClient(Long userId, CallSessionResponseDTO.CallSessionTotalAbuseCntDTO sessionTotalAbuseCnt);
+	void registerUserSession(Long userId, WebSocketSession session);
+}
