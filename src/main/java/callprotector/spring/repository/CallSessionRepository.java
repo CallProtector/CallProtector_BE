16ab68a7a9842faa4d693bf2dd6372c93f76ec87
ose0221 <ose0221@naver.com>
2025-08-02 21:12:37 +0900

    :sparkles: Feat: userId와 callSessionId로 객체 조회하는 메서드 추가

--- src/main/java/callprotector/spring/repository/CallSessionRepository.java
+++ src/main/java/callprotector/spring/repository/CallSessionRepository.java
@@ -1,8 +1,11 @@
 package callprotector.spring.repository;
 
+import java.util.Optional;
+
 import callprotector.spring.domain.CallSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CallSessionRepository extends JpaRepository<CallSession, Long>, CallSessionRepositoryCustom {
     long countByCallSessionCodeStartingWith(String prefix);
+    Optional<CallSession> findByIdAndUserId(Long id, Long userId);
 }
