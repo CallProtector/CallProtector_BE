6eafb706bc76e602235cee7a9a4a8c1db56439f9
ose0221 <ose0221@naver.com>
2025-08-02 17:42:19 +0900

    :sparkles: Feat: @UserId 어노테이션 동작 확인용 API 추가

---
+++ src/main/java/callprotector/spring/web/controller/TestController.java
@@ -0,0 +1,21 @@
+package callprotector.spring.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import callprotector.spring.annotation.UserId;
+import callprotector.spring.apiPayload.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class TestController {
+
+	@GetMapping("/api/test/my-info")
+	public ApiResponse<String> getMyInfo(@UserId Long userId) {
+		log.info("인증된 사용자의 ID: {}", userId);
+		return ApiResponse.onSuccess("User ID " + userId + "님의 정보를 성공적으로 조회했습니다.");
+	}
+}
