da976b33492c00f3cb96dca04294f632a79f4341
ose0221 <ose0221@naver.com>
2025-08-02 18:28:49 +0900

    :recycle: Refactor: 불필요한 주석 제거

--- src/main/java/callprotector/spring/repository/UserRepository.java
+++ src/main/java/callprotector/spring/repository/UserRepository.java
@@ -10,12 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long id);
 
-    // 일단 이메일 중복 체크 메서드 추가추가
     boolean existsByEmail(String email);
 
-    // 이메일로 사용자 조회
     Optional<User> findByEmail(String email);
-
-    // userId를 추가해야되나 고민이네.. ㅇㅅㅇ
-    // Optional<User> findByUserId(String userId);
 }
