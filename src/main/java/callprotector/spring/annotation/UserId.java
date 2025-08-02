17e3a9e427188db78160ce5957087e58997c672e
ose0221 <ose0221@naver.com>
2025-08-02 17:41:26 +0900

    :sparkles: Feat: @UserId 커스텀 어노테이션 구현

---
+++ src/main/java/callprotector/spring/annotation/UserId.java
@@ -0,0 +1,17 @@
+package callprotector.spring.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import io.swagger.v3.oas.annotations.Parameter;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression="#this")
+@Parameter(hidden = true)
+public @interface UserId {
+}
