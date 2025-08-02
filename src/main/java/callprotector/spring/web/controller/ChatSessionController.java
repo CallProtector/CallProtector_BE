ca5577c3113fc40e316f350e4473b5f56bae8542
ose0221 <ose0221@naver.com>
2025-08-02 18:31:43 +0900

    :recycle: Refactor: 인증된 사용자 정보 주입 방식 변경

--- src/main/java/callprotector/spring/web/controller/ChatSessionController.java
+++ src/main/java/callprotector/spring/web/controller/ChatSessionController.java
@@ -1,16 +1,17 @@
 package callprotector.spring.web.controller;
 
+import callprotector.spring.annotation.UserId;
 import callprotector.spring.apiPayload.ApiResponse;
 import callprotector.spring.domain.User;
 import callprotector.spring.service.ChatSessionService.ChatSessionService;
 import callprotector.spring.service.UserService.UserService;
 import callprotector.spring.web.dto.response.ChatSessionResponseDTO;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,10 +22,10 @@ public class ChatSessionController {
     private final UserService userService;
 
     @PostMapping
-    public ApiResponse<ChatSessionResponseDTO.ChatSessionResponse> createSession(Principal principal) {
-        String email = principal.getName();  // JWT에서 추출된 email 값
-        User user = userService.getUserByEmail(email); // UserService에서 조회
+    public ApiResponse<ChatSessionResponseDTO.ChatSessionResponse> createSession(
+        @UserId Long userId
+    ) {
+        User user = userService.getUserById(userId);
         return ApiResponse.onSuccess(chatSessionService.createSession(user));
     }
-
 }
