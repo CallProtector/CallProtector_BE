ed5141101496ee0c13be13235ef3315eeb910f6f
ose0221 <ose0221@naver.com>
2025-08-04 01:58:46 +0900

    :sparkles: Feat: callSessionInfo 반환 메서드 추가

--- src/main/java/callprotector/spring/service/CallSessionService/CallSessionService.java
+++ src/main/java/callprotector/spring/service/CallSessionService/CallSessionService.java
@@ -1,26 +1,33 @@
 package callprotector.spring.service.CallSessionService;
 
 import callprotector.spring.domain.CallSession;
-import callprotector.spring.domain.User;
 import callprotector.spring.web.dto.request.CallSessionRequestDTO;
 import callprotector.spring.web.dto.response.CallSessionResponseDTO;
 
 public interface CallSessionService {
-    Long createCallSession(User user, CallSessionRequestDTO.CallSessionMakeDTO dto );
-    CallSessionResponseDTO.CallSessionInfoDTO getCallSessionInfo(Long callSessionId);
     CallSession getCallSession(Long callSessionId);
+
     void incrementTotalAbuseCnt(Long callSessionId);
+
     void forceTerminateCall(CallSession callSession);
+
     CallSessionResponseDTO.CallSessionPagingDTO getCallSessions(String sortBy, String order, Long cursorId, int size);
+
     CallSessionResponseDTO.CallSessionPagingDTO getSessionsByAbuseCategory(String category, Long cursorId, int size, String order);
+
     CallSessionResponseDTO.CallSessionPagingDTO searchCallSessions(String keyword, String category, String order, Long cursorId, int size);
+
     CallSessionResponseDTO.CallSessionDetailResponseDTO getUserCallSessionDetail(Long callSessionId, Long userId);
 
     String generateSummaryByOpenAi(Long callSessionId, Long userId);
+
     CallSessionResponseDTO.CallSessionSummaryResponseDTO createCallSessionSummaryByOpenAi(Long callSessionId, Long userId);
 
     String generateSummaryByGemini(Long callSessionId, Long userId);
+
     CallSessionResponseDTO.CallSessionSummaryResponseDTO createCallSessionSummaryByGemini(Long callSessionId, Long userId);
 
     void updateEndedAt(Long callSessionId);
-}
+
+    CallSessionResponseDTO.CallSessionInfoDTO registerAcceptedCall(CallSessionRequestDTO.CallSessionMakeDTO dto, Long userId);
+    }
