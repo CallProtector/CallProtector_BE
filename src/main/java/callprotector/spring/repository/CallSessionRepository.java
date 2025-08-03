90daa0dfcc088ac2cc8231c9f95f8681af053ec5
ose0221 <ose0221@naver.com>
2025-08-04 01:53:36 +0900

    :sparkles: Feat: twilioCallSid로 callSession 객체 조회 메서드 구현

--- src/main/java/callprotector/spring/repository/CallSessionRepository.java
+++ src/main/java/callprotector/spring/repository/CallSessionRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CallSessionRepository extends JpaRepository<CallSession, Long>, CallSessionRepositoryCustom {
     long countByCallSessionCodeStartingWith(String prefix);
     Optional<CallSession> findByIdAndUserId(Long id, Long userId);
+    boolean existsByTwilioCallSid(String twilioCallsid);
 }
