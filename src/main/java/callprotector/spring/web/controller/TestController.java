b6d79740805752a61dd4963908042f9f8bba3627
SONG <dlthdal07@gmail.com>
2025-08-04 11:36:05 +0900

    🚚 Chore: annotation 패키지 global.annotation으로 이동 #95

--- src/main/java/callprotector/spring/web/controller/TestController.java
+++ src/main/java/callprotector/spring/web/controller/TestController.java
@@ -3,7 +3,7 @@ package callprotector.spring.web.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import callprotector.spring.annotation.UserId;
+import callprotector.spring.global.annotation.UserId;
 import callprotector.spring.apiPayload.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
