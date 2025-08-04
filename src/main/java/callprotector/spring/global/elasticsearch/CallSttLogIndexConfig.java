26f0d93015b8d106dd57b8f87bfdcb9e3d0597bb
SONG <dlthdal07@gmail.com>
2025-08-04 11:45:55 +0900

    🚚 Chore: elasticsearch 패키지 global.elasticsearch 패키지로 이동 #95

--- src/main/java/callprotector/spring/global/elasticsearch/CallSttLogIndexConfig.java
+++ src/main/java/callprotector/spring/global/elasticsearch/CallSttLogIndexConfig.java
@@ -1,4 +1,4 @@
-package callprotector.spring.elasticsearch;
+package callprotector.spring.global.elasticsearch;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
