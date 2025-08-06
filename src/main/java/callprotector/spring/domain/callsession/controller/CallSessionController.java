674ecb0ee62ef880d6627403960bc4eafad56321
ose0221 <ose0221@naver.com>
2025-08-06 19:31:29 +0900

    :sparkles: Feat : CallSession api path 수정

--- src/main/java/callprotector/spring/domain/callsession/controller/CallSessionController.java
+++ src/main/java/callprotector/spring/domain/callsession/controller/CallSessionController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/sessions")
+@RequestMapping("/api/call-sessions")
 public class CallSessionController {
 
     private final CallSessionService callSessionService;
