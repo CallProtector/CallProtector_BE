0f0af546c9bbdf58c58b58b27df855c7b3e07c2e
ose0221 <ose0221@naver.com>
2025-08-07 03:06:29 +0900

    🚀 Deploy: CORS 해결을 위해 배포 주소 추가

--- src/main/java/callprotector/spring/global/config/WebMvcConfig.java
+++ src/main/java/callprotector/spring/global/config/WebMvcConfig.java
@@ -11,7 +11,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://192.168.35.77:3000", "https://pet-pipefish-friendly.ngrok-free.app", "https://oneum-livid.vercel.app/")
+                .allowedOrigins("http://localhost:3000",
+                    "http://192.168.35.77:3000",
+                    "https://pet-pipefish-friendly.ngrok-free.app",
+                    "https://oneum-livid.vercel.app/",
+                    "https://callprotect.site"
+                )
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
