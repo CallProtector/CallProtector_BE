8980c347df062a883fb36cdf40817247867d4937
SONG <dlthdal07@gmail.com>
2025-08-08 02:29:11 +0900

    :sparkles: Feat: CallSessionId 기준 폭언 유형 존재 여부 조회 repository 구현 #145

--- src/main/java/callprotector/spring/domain/abuse/repository/AbuseTypeLogRepository.java
+++ src/main/java/callprotector/spring/domain/abuse/repository/AbuseTypeLogRepository.java
@@ -8,4 +8,10 @@ import java.util.List;
 
 public interface AbuseTypeLogRepository extends JpaRepository<AbuseTypeLog, Long> {
     List<AbuseTypeLog> findByAbuseLog(AbuseLog abuseLog);
+
+    boolean existsByAbuseLog_CallLog_CallSession_IdAndAbuseType_VerbalAbuseTrue(Long sessionId);
+
+    boolean existsByAbuseLog_CallLog_CallSession_IdAndAbuseType_SexualHarassTrue(Long sessionId);
+
+    boolean existsByAbuseLog_CallLog_CallSession_IdAndAbuseType_ThreatTrue(Long sessionId);
 }
