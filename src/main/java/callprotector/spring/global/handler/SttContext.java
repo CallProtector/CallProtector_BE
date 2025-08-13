d00a5951624fd978a534c26da31b145d884e73e7
ose0221 <ose0221@naver.com>
2025-08-13 21:58:56 +0900

    :recycle: Refactor: 불필요한 주석 제거

--- src/main/java/callprotector/spring/global/handler/SttContext.java
+++ src/main/java/callprotector/spring/global/handler/SttContext.java
@@ -258,12 +258,7 @@ public class SttContext {
 				// 세션 내 기존 욕설 여부 확인
 				boolean hasAbuseInSttLog = callSttLogService.hasAbuseInSession(callSessionId);
 
-				// // FastAPI를 통한 욕설 분석
-				// var inboundResult = fastClient.sendTextToFastAPI(finalTranscript);
-				// log.info("⚠️ [{}] INBOUND 욕설 감지 결과 → isAbuse: {}, type: {}",
-				// 	CallTrack.INBOUND, inboundResult.isAbuse(), inboundResult.getType());
-
-				boolean finalAbuse = hasAbuseInSttLog; //  || inboundResult.isAbuse();
+				boolean finalAbuse = hasAbuseInSttLog;
 
 				String finalAbuseType = hasAbuseInSttLog
 					? callSttLogService.getAbuseTypesBySessionId(callSessionId)
