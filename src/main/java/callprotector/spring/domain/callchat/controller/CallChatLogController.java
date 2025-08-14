9d0f159b85854ba9e07f2af683e6eb031ad5fd4c
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-08-14 18:40:38 +0900

    ♻️ Refactor: CallChatSessionController 세션 목록 조회 api 완성 + @operation추가

--- src/main/java/callprotector/spring/domain/callchat/controller/CallChatLogController.java
+++ src/main/java/callprotector/spring/domain/callchat/controller/CallChatLogController.java
@@ -6,6 +6,7 @@ import callprotector.spring.domain.callchat.service.CallChatLogService;
 import callprotector.spring.domain.callchat.service.CallChatSessionService;
 import callprotector.spring.global.annotation.UserId;
 import callprotector.spring.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +20,12 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/call-chat-log")
+@Tag(name = "CallChatLog", description = "상담별 챗봇 로그 관련 API")
 public class CallChatLogController {
 
     private final CallChatSessionService callChatSessionService;
     private final CallChatLogService callChatLogService;
+
 
     @GetMapping("/session/{sessionId}")
     public ApiResponse<List<CallChatLogResponseDTO.CallChatLogResponse>> getLogs(
--- src/main/java/callprotector/spring/domain/callchat/controller/CallChatSessionController.java
+++ src/main/java/callprotector/spring/domain/callchat/controller/CallChatSessionController.java
@@ -4,6 +4,8 @@ import callprotector.spring.domain.callchat.dto.response.CallChatSessionResponse
 import callprotector.spring.domain.callchat.service.CallChatSessionService;
 import callprotector.spring.global.annotation.UserId;
 import callprotector.spring.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -14,18 +16,16 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/call-chat-sessions")
+@Tag(name = "CallChatSession", description = "상담별 챗봇 세션 관련 API")
 public class CallChatSessionController {
 
     private final CallChatSessionService callChatSessionService;
 
-
+    @Operation(summary = "상담별 채팅 세션 목록 조회 API", description = "유저에 해당하는 상담별 챗봇 채팅 세션을 조회합니다.")
     @GetMapping("/list")
     public ApiResponse<List<CallChatSessionResponseDTO.CallChatSessionResponse>> getSessionList(@UserId Long userId) {
         return ApiResponse.onSuccess(callChatSessionService.getSessionListDtoByUserId(userId));
     }
-
-
-
 
 
 }
