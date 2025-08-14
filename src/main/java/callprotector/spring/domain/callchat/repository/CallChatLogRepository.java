d60846774212ec742e6530e368f91ba5c73bc52b
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-08-14 17:47:41 +0900

    ♻️ Refactor: callchat 리포지터리 파일 이동

--- src/main/java/callprotector/spring/domain/callchat/repository/CallChatLogRepository.java
+++ src/main/java/callprotector/spring/domain/callchat/repository/CallChatLogRepository.java
@@ -1,4 +1,4 @@
-package callprotector.spring.domain.chat.repository;
+package callprotector.spring.domain.callchat.repository;
 
 import callprotector.spring.domain.callchat.entity.CallChatLog;
 import org.springframework.data.jpa.repository.JpaRepository;
