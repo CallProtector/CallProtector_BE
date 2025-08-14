f3f9eaf3942b2bf32f914b40a054fab584625611
SONG <dlthdal07@gmail.com>
2025-08-14 16:43:03 +0900

    :sparkles: Feat: AbuseController Swagger 태그 추가 및 Operation 설명 통일 #157

--- src/main/java/callprotector/spring/domain/abuse/controller/AbuseController.java
+++ src/main/java/callprotector/spring/domain/abuse/controller/AbuseController.java
@@ -5,18 +5,23 @@ import callprotector.spring.global.apiPayload.ApiResponse;
 import callprotector.spring.domain.abuse.service.AbuseService;
 import callprotector.spring.domain.abuse.dto.request.AbuseRequestDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/abuse")
 @RequiredArgsConstructor
+@RequestMapping("/api/abuse")
+@Tag(name = "Abuse", description = "폭언 필터링 API")
 public class AbuseController {
 
     private final AbuseService abuseService;
 
-    @Operation(summary = "욕설 필터링 API", description = "욕설 필터링 API입니다.")
+    @Operation(
+            summary = "폭언 필터링 API",
+            description = "입력된 텍스트에 대해 폭언(욕설, 성희롱, 협박) 여부를 판별합니다."
+    )
     @PostMapping("/filter")
     public ApiResponse<AbuseResponseDTO.AbuseFilterDTO> filterAbuse(
             @RequestBody AbuseRequestDTO.AbuseFilterDTO request) {
