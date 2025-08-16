034461a99a4abc7f5e5a45424678e1691071c312
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-08-17 01:47:35 +0900

    ♻️ Refactor: CallChatLogResponseDTO CallSessionId 필드 제거

--- src/main/java/callprotector/spring/domain/callchat/dto/response/CallChatLogResponseDTO.java
+++ src/main/java/callprotector/spring/domain/callchat/dto/response/CallChatLogResponseDTO.java
@@ -17,7 +17,6 @@ public class CallChatLogResponseDTO {
     @AllArgsConstructor
     public static class CallChatLogResponse {
         private Long id;
-        private Long callSessionId;
         private String question;
         private String answer;
         private List<SourcePageDTO.SourcePage> sourcePages;
