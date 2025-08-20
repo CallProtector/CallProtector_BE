2bfc835c614f9b656c1296e9dc5e10395bf8fe46
ose0221 <ose0221@naver.com>
2025-08-20 12:40:27 +0900

    :recycle: Refactor: 채팅 엔티티에서 중복 의미 필드 제거

--- src/main/java/callprotector/spring/domain/chat/entity/ChatSession.java
+++ src/main/java/callprotector/spring/domain/chat/entity/ChatSession.java
@@ -5,7 +5,6 @@ import callprotector.spring.global.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,16 +23,11 @@ public class ChatSession extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    private LocalDateTime startTime;
-
-    private LocalDateTime endTime;
-
     @Column(nullable = false)
     private Integer status; // 1. 진행 중, 2:종료
 
     @Setter
-    private String title; // title 필드 추가
-
+    private String title;
 
     @Builder.Default
     @OneToMany(mappedBy = "chatSession", cascade = CascadeType.ALL, orphanRemoval = true)
