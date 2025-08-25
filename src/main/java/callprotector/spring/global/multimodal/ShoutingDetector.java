d85687900185c282dc642497d249b892dd1a14e3
SONG <dlthdal07@gmail.com>
2025-08-25 20:38:45 +0900

    :recycle: Refactor: 멀티 모달 기본 설정값으로 수정

--- src/main/java/callprotector/spring/global/multimodal/ShoutingDetector.java
+++ src/main/java/callprotector/spring/global/multimodal/ShoutingDetector.java
@@ -49,10 +49,10 @@ public class ShoutingDetector {
 	private float lastKnownPitch = -1.0f;
 
 	private static final double BASELINE_PERIOD_SECONDS = 1.5;
-	private static final double PITCH_INCREASE_FACTOR_HIGH = 1.03; // TODO: 피치 변화율1
-	private static final double PITCH_INCREASE_FACTOR_LOW = 1.05; // TODO: 피치 변화율2
+	private static final double PITCH_INCREASE_FACTOR_HIGH = 1.15; // TODO: 피치 변화율1
+	private static final double PITCH_INCREASE_FACTOR_LOW = 1.69; // TODO: 피치 변화율2
 	private static final double PITCH_BOUNDARY = 165.0; // 피치 높낮이 구분 기준
-	private static final double DB_BOUNDARY = 0.1; // 데시벨 증가 경계값 // TODO: 세기 변화량
+	private static final double DB_BOUNDARY = 20.0; // 데시벨 증가 경계값 // TODO: 세기 변화량
 
 
 	@Setter
