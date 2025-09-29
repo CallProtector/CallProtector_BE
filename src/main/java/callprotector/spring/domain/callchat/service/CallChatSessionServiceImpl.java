e575688df2199fe6d637bb9e0b773eb429ac42b4
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-09-29 15:26:40 +0900

    ♻️Refactor: CallChatSession 조회 서비스 로직 정렬메소드 수정

--- src/main/java/callprotector/spring/domain/callchat/service/CallChatSessionServiceImpl.java
+++ src/main/java/callprotector/spring/domain/callchat/service/CallChatSessionServiceImpl.java
@@ -49,7 +49,7 @@ public class CallChatSessionServiceImpl implements CallChatSessionService {
     @Override
     @Transactional(readOnly = true)
     public List<CallChatSessionResponseDTO.CallChatSessionResponse> getSessionListDtoByUserId(Long userId) {
-        return callChatSessionRepository.findByUserIdOrderByIdDesc(userId).stream()
+        return callChatSessionRepository.findByUserIdOrderByLastUserQuestionAtDesc(userId).stream()
                 .map(session -> CallChatSessionResponseDTO.CallChatSessionResponse.builder()
                         .sessionId(session.getId())
                         .createdAt(session.getCreatedAt().toString()) // BaseEntity.getCreatedAt()
