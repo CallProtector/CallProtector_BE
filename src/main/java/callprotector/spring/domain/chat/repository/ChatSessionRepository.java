f77cb70824c8f04555a4ce2582b9a5892122b530
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-09-29 15:21:43 +0900

    ♻️ Refactor: ChatSessionRepository  lastUserQuestionAt기준 내림차순 정렬
    Refactor: ChatSessionRepository  lastUserQuestionAt기준 내림차순 정렬

--- src/main/java/callprotector/spring/domain/chat/repository/ChatSessionRepository.java
+++ src/main/java/callprotector/spring/domain/chat/repository/ChatSessionRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {
 
-    List<ChatSession> findByUserIdOrderByCreatedAtDesc(Long userId);
+    List<ChatSession> findByUserIdOrderByLastUserQuestionAtDesc(Long userId);
 
 }
