50db69cf897e6d33b97e6d421fe58bf718057f9d
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-09-29 15:25:17 +0900

    ♻️Refactor: ChatSession 조회 api 정렬메소드 수정

--- src/main/java/callprotector/spring/domain/chat/service/ChatSessionServiceImpl.java
+++ src/main/java/callprotector/spring/domain/chat/service/ChatSessionServiceImpl.java
@@ -63,7 +63,7 @@ public class ChatSessionServiceImpl implements ChatSessionService{
     @Override
     @Transactional(readOnly = true)
     public List<ChatSessionResponseDTO.ChatSessionResponse> getSessionList(Long userId) {
-        return chatSessionRepository.findByUserIdOrderByCreatedAtDesc(userId).stream()
+        return chatSessionRepository.findByUserIdOrderByLastUserQuestionAtDesc(userId).stream()
                 .map(session -> ChatSessionResponseDTO.ChatSessionResponse.builder()
                         .sessionId(session.getId())
                         .title(session.getTitle())
