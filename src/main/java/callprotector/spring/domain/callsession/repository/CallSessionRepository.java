0e94a110d1e072df230e07a51665e6a43654e10d
SONG <dlthdal07@gmail.com>
2025-10-05 08:49:42 +0900

    :sparkles: Feat: ConferenceSid 조회 repository 구현 #184

--- src/main/java/callprotector/spring/domain/callsession/repository/CallSessionRepository.java
+++ src/main/java/callprotector/spring/domain/callsession/repository/CallSessionRepository.java
@@ -4,10 +4,15 @@ import java.util.Optional;
 
 import callprotector.spring.domain.callsession.entity.CallSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CallSessionRepository extends JpaRepository<CallSession, Long>, CallSessionRepositoryCustom {
     long countByCallSessionCodeStartingWith(String prefix);
     Optional<CallSession> findByIdAndUserId(Long id, Long userId);
     boolean existsByTwilioCallSid(String twilioCallsid);
     Optional<CallSession> findByTwilioCallSid(String twilioCallsid);
+
+    @Query("SELECT c.twilioConferenceSid FROM CallSession c WHERE c.id = :callSessionId")
+    Optional<String> findConferenceSidById(@Param("callSessionId") Long callSessionId);
 }
