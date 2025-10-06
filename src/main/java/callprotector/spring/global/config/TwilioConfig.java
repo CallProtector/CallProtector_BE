702630827ef43d40fab1fd9bdfde92ca0f33e433
SONG <dlthdal07@gmail.com>
2025-10-06 22:10:13 +0900

    :bug: Fix: twilio API 인증 오류 해결을 위한 설정 추가 #184

--- src/main/java/callprotector/spring/global/config/TwilioConfig.java
+++ src/main/java/callprotector/spring/global/config/TwilioConfig.java
@@ -1,11 +1,13 @@
 package callprotector.spring.global.config;
 
 import com.twilio.Twilio;
+import com.twilio.http.TwilioRestClient;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -19,11 +21,24 @@ public class TwilioConfig {
     @Value("${twilio.auth.token}")
     private String twilioAuthToken;
 
+    @Value("${twilio.api.key}")
+    private String twilioApiKey;
+
+    @Value("${twilio.api.secret}")
+    private String twilioApiSecret;
+
     @PostConstruct
     public void init() {
         Twilio.init(
                 getTwilioAccountSid(),
                 getTwilioAuthToken()
         );
+    }
+
+    @Bean
+    public TwilioRestClient twilioRestClient() {
+        return new TwilioRestClient.Builder(twilioApiKey, twilioApiSecret)
+                .accountSid(twilioAccountSid)
+                .build();
     }
 }
