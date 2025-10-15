deef34a6623577671d45e05f20aacc21953f0aea
ose0221 <ose0221@naver.com>
2025-10-16 01:10:24 +0900

    :recycle:  Refactor: 멀티모달 경계값 수정 테스트

--- src/main/java/callprotector/spring/global/multimodal/ShoutingDetector.java
+++ src/main/java/callprotector/spring/global/multimodal/ShoutingDetector.java
@@ -49,10 +49,10 @@ public class ShoutingDetector {
 	private float lastKnownPitch = -1.0f;
 
 	private static final double BASELINE_PERIOD_SECONDS = 1.5;
-	private static final double PITCH_INCREASE_FACTOR_HIGH = 1.15; // TODO: 피치 변화율1
+	private static final double PITCH_INCREASE_FACTOR_HIGH = 1.08; // TODO: 피치 변화율1
 	private static final double PITCH_INCREASE_FACTOR_LOW = 1.69; // TODO: 피치 변화율2
 	private static final double PITCH_BOUNDARY = 165.0; // 피치 높낮이 구분 기준
-	private static final double DB_BOUNDARY = 20.0; // 데시벨 증가 경계값 // TODO: 세기 변화량
+	private static final double DB_BOUNDARY = 7.0; // 데시벨 증가 경계값 // TODO: 세기 변화량
 
 	private static final double DEFAULT_BASELINE_PITCH_HZ = 150.0; // TODO: default pitch
 	private static final double DEFAULT_BASELINE_VOLUME_DB = 30.0; // TODO: default dB
